ce635e5904c36b0173946ad3b1191228ffc066f2
IhorKlymenko <ihorklymenkoc@gmail.com>
2022-07-29 19:14:58 +0300

    Add Login in Hub

---
+++ API_Core/IServerHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace API_Core
+{
+    public interface IServerHub
+    {
+        Task<bool> LoginAsync(string login, string pass);
+        Task SendMessageAsync(string message);
+    }
+}
